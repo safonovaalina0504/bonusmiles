726c2f97f81de3d7d662bfee01c56ae9883d9331
AlinaS <safonova.alina2012@ya.ru>
2025-08-13 23:52:16 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int price = 50_000;
+        int price = 10_000;
         int rublesPerMile = 20;
 
 
